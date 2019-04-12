b249c2d4511bd65ae0cd151095d384da23e98931
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-04-12 09:47:51 +0300

    added stroke

--- TestRoomAlgorithm/role.harvester.js
+++ TestRoomAlgorithm/role.harvester.js
@@ -3,15 +3,15 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
-            creep.say('🔄 mine');
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[creep.memory.sourceNumber]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[creep.memory.sourceNumber]);
+                creep.moveTo(sources[creep.memory.sourceNumber], {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.say('🔄 mine');
             }
         }
         else {
             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+                creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffffff'}});
                 creep.say('♻️ transfer');
             }
         }
