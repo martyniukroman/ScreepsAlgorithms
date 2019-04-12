d8a80b0d3c6874b262d40dbc3cdadc71f3131870
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-04-12 14:58:12 +0300

    harvester updated

--- TestRoomAlgorithm/role.harvester.js
+++ TestRoomAlgorithm/role.harvester.js
@@ -2,17 +2,27 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        
+         if(creep.memory.harvesting && creep.carry.energy == 0) {
+            creep.memory.harvesting = false;
+                creep.say('🔄 mine');
+        }
+        if(!creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.harvesting = true;
+                    creep.say('♻️');
+        }
+        
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[creep.memory.sourceNumber]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[creep.memory.sourceNumber], {visualizePathStyle: {stroke: '#ffffff'}});
-                creep.say('🔄 mine');
+                
             }
         }
         else {
             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffffff'}});
-                creep.say('♻️ transfer');
+                
             }
         }
     }
