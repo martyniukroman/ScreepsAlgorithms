8439b71f6f87cafd61f027f4def1db3ce95507e5
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-04-26 09:26:08 +0300

    fix

--- TestRoomAlgorithm/main.js
+++ TestRoomAlgorithm/main.js
@@ -93,7 +93,6 @@ module.exports.loop = function () {
             var closestHostile =  towers[i].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                if (closestHostile) {
                    towers[i].attack(closestHostile);
-                   return;
                }
                var closestDamagedStructure =  towers[i].pos.findClosestByRange(FIND_STRUCTURES, {
                    filter: (structure) => structure.hits < structure.hitsMax
